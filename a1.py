41f92a103f4ac21875118fd49c298a46ace15824
admin <admin@admins-MacBook-Pro.local>
2024-02-20 10:16:29 -0500

    file rename

--- a1.py
+++ a1.py
@@ -1,6 +1,6 @@
 #Name: Itai Lavie
 #Email: itai.lavie47@myhunter.cuny.edu
-#Date: February 10, 2024
-#This program completes assignment 4
+#Date: February 1, 2024
+#This program completes assignment 1
 
 print("Hello, World!")
