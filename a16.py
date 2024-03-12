a324d5201c6fcdf13351e0ba98b106e0bf7a8c4f
admin <admin@admins-MBP.lan>
2024-03-12 01:49:11 -0400

    a17

--- a16.py
+++ a16.py
@@ -1,10 +1,24 @@
 #Name: Itai Lavie
 #Email: itai.lavie47@myhunter.cuny.edu
 #Date: March 13, 2024
-#This program completes assignment 18
+#This program completes assignment 17
 
-hours = int(input("Enter number of hours: "))
-days = hours // 24
-leftover = hours % 24
-print("Days:", days)
-print("Hours:", leftover)
+import matplotlib.pyplot as plt
+import numpy as np
+
+size = int(input("Enter the size: "))
+output_file = input("Enter output file: ")
+
+purple = [128/255, 0, 128/255]
+yellow = [1, 1, 0]
+
+image = np.zeros((size, size, 3))
+
+stripe_width = size // 10
+for i in range(0, size, stripe_width*2):
+    image[i:i+stripe_width, :, :] = purple
+    if i+stripe_width < size:
+        image[i+stripe_width:i+(stripe_width*2), :, :] = yellow
+
+plt.imsave(output_file, image)
+
