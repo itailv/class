3f098282e682a4b857deb5525d09280e2b020b6c
admin <admin@admins-MBP.lan>
2024-03-12 01:50:53 -0400

    a19

--- a16.py
+++ a16.py
@@ -1,24 +1,13 @@
 #Name: Itai Lavie
 #Email: itai.lavie47@myhunter.cuny.edu
 #Date: March 13, 2024
-#This program completes assignment 17
+#This program completes assignment 19
 
-import matplotlib.pyplot as plt
-import numpy as np
+# Prompt the user for the number of kilometers
+kilometers = float(input("Enter the number of kilometers: "))
 
-size = int(input("Enter the size: "))
-output_file = input("Enter output file: ")
+# Convert kilometers to miles
+miles = 0.621371 * kilometers
 
-purple = [128/255, 0, 128/255]
-yellow = [1, 1, 0]
-
-image = np.zeros((size, size, 3))
-
-stripe_width = size // 10
-for i in range(0, size, stripe_width*2):
-    image[i:i+stripe_width, :, :] = purple
-    if i+stripe_width < size:
-        image[i+stripe_width:i+(stripe_width*2), :, :] = yellow
-
-plt.imsave(output_file, image)
-
+# Print out the number of miles
+print(f"{kilometers} kilometers is equal to {miles} miles.")
